6609ea88c10b4b9de20f6ce4c8e12ac1ff39e96e
Tobias Laving <tobbeman1994@gmail.com>
2017-12-07 14:42:41 +0100

    ee

--- 7, Recursive Circus/Program.cs
+++ 7, Recursive Circus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -11,7 +12,8 @@ namespace _7__Recursive_Circus
         {
             public string name;
             public int weight;
-            public disc[] children;
+            public int totWeight;
+            public List<disc> children;
         }
 
         static disc parseLine(string line)
@@ -43,7 +45,85 @@ namespace _7__Recursive_Circus
 
         static void Main(string[] args)
         {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\input.txt");
+            Console.WriteLine("Reading for file at: " + path);
+            var lines = File.ReadLines(path);
 
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("First: " + first(lines));
+            Console.WriteLine("Run time: " + (DateTime.Now - dt));
+            dt = DateTime.Now;
+            Console.WriteLine("Second: " + second(lines));
+            Console.WriteLine("Run time: " + (DateTime.Now - dt));
+            Console.ReadKey();
+        }
+        static string first(IEnumerable<string> lines)
+        {
+            HashSet<string> hasParent = new HashSet<string>();
+
+            foreach(var line in lines)
+            {
+                string[] segments = line.Split(' ');
+                for (int i = 3; i < segments.Length; i++)
+                {
+                    if (i == segments.Length - 1)
+                        hasParent.Add(segments[i]);
+                    else
+                        hasParent.Add(segments[i].Substring(0, segments[i].Length - 1));
+                }
+            }
+            foreach(var line in lines)
+            {
+                string[] segments = line.Split(' ');
+                if (!hasParent.Contains(segments[0]))
+                    return segments[0];
+            }
+            return "Error";
+        }
+
+        static int second(IEnumerable<string> lines)
+        {
+            Dictionary<string, disc> discMap = new Dictionary<string, disc>();
+            foreach(var line in lines)
+            {
+                string[] segments = line.Split(' ');
+                disc d = new disc();
+                d.name = segments[0];
+                string f = segments[1].Substring(1, segments[1].Length - 2);
+                d.weight = int.Parse(f);
+                d.totWeight = d.weight;
+                discMap.Add(d.name, d);
+            }
+
+            foreach (var line in lines)
+            {
+                string[] segments = line.Split(' ');
+                disc d = new disc();
+                d.name = segments[0];
+                int layerWeight = 0;
+                for (int i = 3; i < segments.Length; i++)
+                {
+                    string name;
+                    if (i == segments.Length - 1)
+                        name = segments[i];
+                    else
+                        name = segments[i].Substring(0, segments[i].Length - 1);
+
+                    disc child = discMap[name];
+
+                    if (i == 2)
+                        layerWeight = child.totWeight;
+
+                    if (child.totWeight != layerWeight)
+                        return child.totWeight - layerWeight;
+
+                    d.totWeight += child.weight;
+                    //Check if weight is total!
+                    d.children.Add(child);
+                }
+                discMap[d.name] = d;
+            }
+            return 0;
         }
     }
 }
